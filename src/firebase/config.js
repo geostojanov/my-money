45f447785ed4cf24d3f5ac89dcf19e8e0ac61f05
George <gestojanov@gmail.com>
2023-07-15 14:21:43 +0200

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwfrBnpLsH70WlhwSkHuKHb7xRDaos9dA",
+  authDomain: "mymoney-ed4b8.firebaseapp.com",
+  projectId: "mymoney-ed4b8",
+  storageBucket: "mymoney-ed4b8.appspot.com",
+  messagingSenderId: "515578593700",
+  appId: "1:515578593700:web:fe59645f64aa9d43e08131"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+
+export { projectFirestore, projectAuth }
