2f135c75da03c8a4071ae766ed7149c1d91728ce
geostojanov <gestojanov@gmail.com>
2025-01-22 14:51:02 +0100

    Navbar components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // pages & components
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
-          
           <Route exact path="/">
             <Home />
           </Route>
-          
-          <Route exact path="/login">
+          <Route path="/login">
             <Login />
           </Route>
-          
-          <Route exact path="/signup">
+          <Route path="/signup">
             <Signup />
           </Route>
-          
         </Switch>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,4 @@
-import './Login.module.css'
-
+import styles from './Login.module.css'
 
 export default function Login() {
   return (
