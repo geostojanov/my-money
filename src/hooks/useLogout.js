d78db6779e2bfe784dae4f420157e6ac9894ca6c
geostojanov <gestojanov@gmail.com>
2025-01-23 16:29:23 +0100

    useLogout hook used

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,8 @@
-import { use, useState } from "react"
+import { useState } from "react"
 import { projectAuth } from "../firebase/config"
 import { useAuthContext } from "./useAuthContext"
 
-const useLogout = () => {
+export const useLogout = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
