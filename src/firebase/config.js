131bedd105fd7fa9f9b8c273d08322fcc389fcd1
geostojanov <gestojanov@gmail.com>
2025-01-23 15:29:40 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwfrBnpLsH70WlhwSkHuKHb7xRDaos9dA",
+  authDomain: "mymoney-ed4b8.firebaseapp.com",
+  projectId: "mymoney-ed4b8",
+  storageBucket: "mymoney-ed4b8.firebasestorage.app",
+  messagingSenderId: "515578593700",
+  appId: "1:515578593700:web:fe59645f64aa9d43e08131"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
