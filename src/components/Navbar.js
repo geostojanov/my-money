0dd2a31a9dabe0f23dcf9f929ced7fd0fe19110f
geostojanov <gestojanov@gmail.com>
2025-01-25 12:13:24 +0100

    Conditionally Showing User Content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,31 @@ import { useLogout } from '../hooks/useLogout'
 
 // styles
 import styles from './Navbar.module.css'
+import { useAuthContext } from "../hooks/useAuthContext"
 
 export default function Navbar() {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
   
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
         
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        
-        <li>
-          <button className="btn" onClick={logout}>Logout</button>
-        </li>
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>  
+        )}
+        {user && (
+          <>
+            <li>hello, {user.displayName}</li>  
+            <li>
+              <button className="btn" onClick={logout}>Logout</button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
